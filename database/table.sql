9b6818df6120b2e005327dee9875135f1eeffe27
FarhanSadaf <imfarhansadaf@gmail.com>
2021-11-18 01:01:30 +0600

    Updated table

--- database/table.sql
+++ database/table.sql
@@ -1,5 +1,13 @@
 create table Student {
-  name varchar2,
-  roll varchar2,
-  result number(3, 2)
+  id 		number(7) 		PRIMARY KEY,
+  name 		varchar2(30),
+  roll 		varchar2(10),
+  dept_id 	number(7) 		REFERENCES Department(id),
+  result 	number(3, 2)
+};
+
+create table Department {
+  id 		number(7) 		PRIMARY KEY,
+  name 		varchar2(30),
+  code 		varchar(4)
 };
