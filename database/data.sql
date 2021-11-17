cd2574c7796d378d44ca82f92f47bbca22f83d98
FarhanSadaf <imfarhansadaf@gmail.com>
2021-11-18 01:01:39 +0600

    Updated data

--- database/data.sql
+++ database/data.sql
@@ -1,1 +1,9 @@
-insert into Student values ('Alex', '123456', 2.98);
+insert into Student(id, name, roll, dept_id, result) values (1, 'Farah', '2107032', 3, 3.68);
+insert into Student(id, name, roll, dept_id, result) values (2, 'Sarah', '2007091', 3, 3.92);
+insert into Student(id, name, roll, dept_id, result) values (3, 'Rafid', '2105003', 2, 3.08);
+insert into Student(id, name, roll, dept_id, result) values (4, 'Noah', '2203033', 1, 3.33);
+insert into Student(id, name, roll, dept_id, result) values (5, 'Shaheed', '2103031', 1,  2.96);
+
+insert into Department(id, name, code) values (1, 'EEE', '03');
+insert into Department(id, name, code) values (2, 'ME', '05');
+insert into Department(id, name, code) values (3, 'CSE', '07');
